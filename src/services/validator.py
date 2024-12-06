d108dd6c466fee35aaa40756f66f2bbf4e60e2ed
kingahmadr <ahmadnromiz@gmail.com>
2024-12-07 06:23:02 +0700

    fix: smtp

--- src/services/validator.py
+++ src/services/validator.py
@@ -28,67 +28,65 @@ class Validator:
             return jsonify({"error": "Invalid email", "message": str(e)}), 400
     
     @staticmethod
-    def send_verification_email(user_email, code):
-        """Send a verification email with the code."""
-        sender_email = os.getenv('SENDER_EMAIL')
-        smtp_user = os.getenv('SMTP_USER')
-        receiver_email = user_email
+    def send_verification_email(user_email, code):                            
+        """Send a verification email with the code."""                        
+        sender_email = os.getenv('SENDER_EMAIL')                              
+        smtp_user = os.getenv('SMTP_USER')                                    
+        receiver_email = user_email                                           
         password = os.getenv('SMTP_PASSWORD')  # Use an app password for Gmail or another service
-
-        subject = "Email Verification"
-        body = f"Your verification code is: {code}\nThis code will expire in 15 minutes."
-
-        msg = MIMEMultipart()
-        msg['From'] = sender_email
-        msg['To'] = receiver_email
-        msg['Subject'] = subject
-
+                                                                                                 
+        subject = "Email Verification"                                                           
+        body = f"Your verification code is: {code}\nThis code will expire in 15 minutes."        
+                                                                                                 
+        msg = MIMEMultipart()                                                                    
+        msg['From'] = sender_email                                                               
+        msg['To'] = receiver_email                                                               
+        msg['Subject'] = subject                                                         
+                                                                                         
         msg.attach(MIMEText(body, 'plain'))
 
-        try:
-            # Establish the server connection (e.g., for Gmail)
-            server = smtplib.SMTP('live.smtp.mailtrap.io', 587)
-            server.starttls()
-            server.login(smtp_user, password)
-            text = msg.as_string()
-            server.sendmail(sender_email, receiver_email, text)
-            server.quit()
-            print("Verification email sent successfully!")
-            return jsonify({"message": "Check your email for the verification code."}), 200
-        except Exception as e:
-            print(f"Error sending email: {e}")
+        try:                                                                                     
+            # Establish the server connection (e.g., for Gmail)                                  
+            server = smtplib.SMTP('live.smtp.mailtrap.io', 587)                                  
+            server.starttls()                                                                    
+            server.login(smtp_user, password)                                                    
+            text = msg.as_string()                                                               
+            server.sendmail(sender_email, receiver_email, text)                                  
+            server.quit()                                                                        
+            print("Verification email sent successfully!")                                       
+            return jsonify({"message": "Check your email for the verification code."}), 200      
+        except Exception as e:                                                                   
+            print(f"Error sending email: {e}")                                                   
             return jsonify({"error": f"Error sending email: {e}"}), 500
 
-    @staticmethod
-    def send_forgot_password_email(user_email, url_link):
-        # Implement the logic to send the forgot password email
-        """Send a verification email with the code."""
-        sender_email = os.getenv('SENDER_EMAIL')
-        smtp_user = os.getenv('SMTP_USER')
-        receiver_email = user_email
+    @staticmethod                                                                                
+    def send_forgot_password_email(user_email, url_link):                                        
+        # Implement the logic to send the forgot password email                                  
+        """Send a verification email with the code."""                                           
+        sender_email = os.getenv('SENDER_EMAIL')                                                 
+        smtp_user = os.getenv('SMTP_USER')                                                       
+        receiver_email = user_email                                                              
         password = os.getenv('SMTP_PASSWORD')  # Use an app password for Gmail or another service
-
-        subject = "Email Forgot Password"
-        body = f"Please click the following link to reset your password: {url_link}"
-
-        msg = MIMEMultipart()
-        msg['From'] = sender_email
-        msg['To'] = receiver_email
-        msg['Subject'] = subject
-
+                                                                                                 
+        subject = "Email Forgot Password"                                                        
+        body = f"Please click the following link to reset your password: {url_link}"             
+                                                                                                 
+        msg = MIMEMultipart()                                                                    
+        msg['From'] = sender_email                                                               
+        msg['To'] = receiver_email                                                               
+        msg['Subject'] = subject                                                                 
+                                                                                                 
         msg.attach(MIMEText(body, 'plain'))
 
-
-        try:
-            # Establish the server connection (e.g., for Gmail)
-            server = smtplib.SMTP('live.smtp.mailtrap.io', 587)
-            server.starttls()
-            server.login(smtp_user, password)
-            text = msg.as_string()
-            server.sendmail(sender_email, receiver_email, text)
-            server.quit()
-            print("Forgot password email sent successfully!")
-            return jsonify({"message": "Check your email for the forgot password link."}), 200
-        except Exception as e:
+        try:                                                                                     
+            # Establish the server connection (e.g., for Gmail)                                  
+            server = smtplib.SMTP('live.smtp.mailtrap.io', 587)                                  
+            server.starttls()                                                                    
+            server.login(smtp_user, password)                                                    
+            text = msg.as_string()                                                               
+            server.sendmail(sender_email, receiver_email, text)                                  
+            server.quit()                                                                        
+            print("Forgot password email sent successfully!")                                    
+            return jsonify({"message": "Check your email for the forgot password link."}), 200   
+        except Exception as e:                                                                   
             print(f"Error sending email: {e}")
-            return jsonify({"error": f"Error sending email: {e}"}), 500
