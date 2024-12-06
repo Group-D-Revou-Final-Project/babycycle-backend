2ebb1e108f5f55529246c490f1b0e11304e2da14
kingahmadr <ahmadnromiz@gmail.com>
2024-12-06 19:01:30 +0700

    update dev template

--- src/config/prod.py
+++ src/config/prod.py
@@ -1,3 +1,4 @@
 from .base import *  # noqa
 import os
 SQLALCHEMY_DATABASE_URI = os.getenv("DB_URI_PROD")
+SQLALCHEMY_TRACK_MODIFICATIONS = False
--- src/config/settings.py
+++ src/config/settings.py
@@ -7,7 +7,7 @@ from flask_seeder import FlaskSeeder
 import os
 from dotenv import load_dotenv
 from flasgger import Swagger
-from flask_login import LoginManager
+# from flask_login import LoginManager
 
 
 
@@ -64,33 +64,21 @@ def create_app(settings_conf=None):
     conf = settings_conf or os.getenv("FLASK_SETTINGS_MODULE")
     app.config.from_object(conf)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URI_PROD')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
     app.config['DEBUG'] = os.getenv('DEBUG', 'True') == 'True'  # Convert from string to boolean
-    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'YourFallbackSecretKey')
+    app.config['SECRET_KEY'] = os.getenv('KEY_SECRET')
 
-    # app.config['DEBUG'] = True
 
     # Initialize the app with extensions
     db.init_app(app)
     migrate.init_app(app, db)
     seeder.init_app(app, db)
 
-    # login_manager = LoginManager(app)
-    # # login_manager.login_view = "/"  # Set the login view endpoint name
-
-    # @login_manager.user_loader
-    # def load_user(user_id):
-    #     from src.model.user import User
-    #     return User.query.get(int(user_id))
-
     api_url='/api/v1'
-    from src.routers.auth import auth_bp
-    app.register_blueprint(auth_bp, name='authentication', url_prefix=api_url + '/auth')
+    from src.routers.register import register_blueprint
 
-
-
+    app.register_blueprint(register_blueprint, url_prefix=api_url + '/users')
+    
+    # http://127.0.0.1:5000/api/v1/users/register
     
     @app.route('/')
     def hello_from_api():
